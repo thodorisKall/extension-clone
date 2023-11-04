aa64f8c07ded0119953ff3e62c1c609105906b81
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-11-04 02:15:01 +0200

    feat: add basic funcionality for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles.css"
+import Weather from "./components/Weather"
+import Day from "./components/Day"
 
 function App() {
-  return <div className='app'>Hello Typescript</div>
+  const [name, setName] = useState<string | null>("")
+  const [city, setCity] = useState<string | null>("")
+  const [savedName, setSavedName] = useState<string | null>("")
+  const [savedCity, setSavedCity] = useState<string | null>("")
+  const [showComponents, setShowComponents] = useState<boolean>(false)
+
+  const handleSave = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setSavedCity(city)
+    setSavedName(name)
+  }
+
+  return (
+    <div className='app'>
+      {!showComponents ? (
+        <>
+          <form>
+            <input
+              type='text'
+              placeholder='Enter your little name'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setName(e.target.value)
+              }
+            />
+            <input
+              type='text'
+              placeholder='Enter the city you prefer'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setCity(e.target.value)
+              }
+            />
+          </form>
+          <button
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              handleSave(e)
+              setShowComponents(true)
+            }}
+          >
+            Goo
+          </button>
+        </>
+      ) : (
+        <>
+          <p>
+            {savedCity}s{savedName}
+          </p>
+          {savedCity && <Weather savedCity={savedCity} />}
+          <Day />
+        </>
+      )}
+    </div>
+  )
 }
 
 export default App

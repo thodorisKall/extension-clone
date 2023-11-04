f43b925a76d1473434a9d446f2b16e85acae7021
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-11-05 01:23:02 +0200

    docs: create Interface for Quote component

---
+++ src/interfaces/QuoteInterface.ts
@@ -0,0 +1,10 @@
+export interface QuoteApiResponse {
+  _id: string
+  author: string
+  content: string
+  tags: string[]
+  authorSlug: string
+  length: number
+  dateAdded: string
+  dateModified: string
+}

f3b234233000252db6e9b562cb0f0479bb2f7057
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-11-05 01:19:42 +0200

    feat: create Quote component

---
+++ src/components/Quote.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { QuoteApiResponse } from "../interfaces/QuoteInterface"
+
+const Quote = () => {
+  const [quotesData, setQuoteData] = useState<QuoteApiResponse | null>(null)
+  const quotesApiUrl = "https://api.quotable.io/random"
+
+  const fetchQuotes = () => {
+    axios
+      .get<QuoteApiResponse>(quotesApiUrl)
+      .then((res) => {
+        setQuoteData(res.data)
+        console.log("Quotes: ", res.data)
+      })
+      .catch((err) => console.log("ERROR at Quotes api", err.message))
+  }
+
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
+
+  return (
+    <div>
+      <h3>{quotesData?.content}</h3>
+      <p>- {quotesData?.author}</p>
+    </div>
+  )
+}
+
+export default Quote
